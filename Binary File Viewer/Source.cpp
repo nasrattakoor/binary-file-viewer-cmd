c4e2f946cf3a4fc6b7239415e906b0074f0f8f06
Nasrat Takoor <nasrattakoor@users.noreply.github.com>
2021-09-11 11:38:53 -0400

    version 1

---
+++ Binary File Viewer/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+#define FIELD_WIDTH 6
+
+using namespace std;
+
+int main()
+{
+	string filePath;
+	cout << "Enter full path of file: ";
+	cin >> filePath;
+
+	ifstream file(filePath, ios_base::in | ios_base::binary);
+	if (!file.is_open())
+	{
+		cout << "File could not be opened." << endl;
+		return -1;
+	}
+
+	char* contents = new char[1024 * 100];
+	file.read(contents, 1024 * 100);
+	//contents[file.gcount()] = '\0';
+	stringstream ss;
+	for (size_t i = 0; i < file.gcount(); i++)
+	{
+		ss << "[" << to_string(contents[i]) << "] ";
+	}
+	cout << ss.str();
+	cin.get();
+}
